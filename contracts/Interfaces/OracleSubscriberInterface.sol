121070f02dd1bc03ef23e59c6d0ee2f36de30d5c
potsables <alextrottier2014@gmail.com>
2018-05-27 18:12:08 -0700

    removed old files

--- contracts/Interfaces/OracleSubscriberInterface.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity 0.4.24;
-
-interface OracleSubscriberInterface {
-    function updateRtcPrice(uint256 _rtcUSD) external returns (bool);
-    function updateEthPrice(uint256 _ethUSD) external returns (bool);
-}
