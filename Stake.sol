f23c163fdcf64f61631f3ad80bdd863e3e2e17b9
potsables <alextrottier2014@gmail.com>
2018-07-07 22:30:06 -0700

    expanding upon the stake system

--- Stake.sol
+++ Stake.sol
@@ -7,8 +7,10 @@ import "./Math/SafeMath.sol";
 contract Stake is Administration {
     using SafeMath for uint256;
 
+    RTCoinInterface constant public RTI = RTCoinInterface(address(0));
     uint256 constant public MULTIPLIER = 10000000000000000;
     uint256 constant public BLOCKHOLDPERIOD = 2103840;
+    uint256 constant public BLOCKSEC = 13;
 
     enum StakeStateEnum { nil, pending, registered, finished }
 
@@ -17,6 +19,8 @@ contract Stake is Administration {
         uint256 stakeAmount;
         uint256 blockLocked;
         uint256 blockUnlocked;
+        uint256 releaseDate;
+        uint256 coinsMinted;
     }
 
     mapping (address => mapping (uint256 => bytes32)) public stakeNumToIDMap;
@@ -24,5 +28,57 @@ contract Stake is Administration {
     mapping (address => mapping (uint256 => StakeStruct)) public stakes;
     mapping (address => uint256) public numberOfStakes;
 
+    modifier isWholeNumber(uint256 _number) {
+        assert(_number % 1 == 0);
+        _;
+    }
     constructor () {}
+
+
+    function depositStake(uint256 _numRTC)
+        external
+        isWholeNumber(_numRTC)
+        returns (bool)
+    {
+        uint256 stakeCount = getStakeCount(msg.sender);
+        (uint256 blockLocked, 
+        uint256 blockReleased, 
+        uint256 releaseDate, 
+        uint256 totalCoinsMinted) = calculateStake(_numRTC);
+        StakeStruct memory ss = StakeStruct({
+            stakeID: keccak256(blockLocked, blockReleased, releaseDate, totalCoinsMinted, stakeCount),
+            stakeAmount: _numRTC,
+            blockLocked: blockLocked,
+            blockUnlocked: blockReleased,
+            releaseDate: releaseDate,
+            coinsMinted: totalCoinsMinted
+        });
+        stakes[msg.sender][stakeCount] = ss;
+        return true;
+    }
+
+
+    // UTILITY FUNCTIONS //
+
+    function calculateStake(uint256 _numRTC) 
+        internal
+        view
+        returns (
+            uint256 blockLocked, 
+            uint256 blockReleased, 
+            uint256 releaseDate, 
+            uint256 totalCoinsMinted
+        ) 
+    {
+        blockLocked = block.number;
+        blockReleased = blockLocked.mul(BLOCKHOLDPERIOD);
+        releaseDate = now.add(BLOCKHOLDPERIOD.mul(1 seconds));
+        totalCoinsMinted = _numRTC.mul(MULTIPLIER);
+        totalCoinsMinted = totalCoinsMinted.div(1 ether);
+    }
+
+    function getStakeCount(address _staker) internal view returns (uint256) {
+        return numberOfStakes[_staker];
+    }
+
 }
