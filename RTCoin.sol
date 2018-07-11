a7e5515016df49aa8d6ae8d1ab70791963cbc957
potsables <alextrottier2014@gmail.com>
2018-07-10 20:12:45 -0700

    rtcoin cleanup

--- RTCoin.sol
+++ RTCoin.sol
@@ -1,12 +1,5 @@
 pragma solidity 0.4.24;
 
-/**
-    [AUTHOR] - Postables
-    Property of RTrade Technology Ltd only. If you wish to use this code you must seek approval from the company first.
-    This is the prototype version of RTCoin. It will not be the final version used by the RTCoin network however
-    it will be directly exchangeable on a 1:1 basis via smart contract automatically at the time RTCoin is released.
-*/
-
 import "./Modules/Administration.sol";
 import "./Math/SafeMath.sol";
 import "./Interfaces/ERC20Interface.sol";
@@ -16,14 +9,14 @@ contract RTCoin is Administration {
 
     using SafeMath for uint256;
 
-    StakeInterface public stake;
+    uint256 constant public INITIALSUPPLY = 61000000000000000000000000;
 
-    address public  stakeContract;
-    address public  mergedMinerValidator;
+    StakeInterface public stake;
+    address public  stakeContractAddress;
+    address public  mergedMinerValidatorAddress;
     string  public  name;
     string  public  symbol;
     uint256 public  totalSupply;
-    uint256 public  initialSupply = 61000000000000000000000000;
     uint8   public  decimals;
     bool    public  transfersFrozen;
 
@@ -36,6 +29,9 @@ contract RTCoin is Administration {
     event TransfersThawed(bool indexed _transfersThawed);
     event ForeignTokenTransfer(address indexed _sender, address indexed _recipient, uint256 _amount);
     event EthTransferOut(address indexed _recipient, uint256 _amount);
+    event MergedMinerValidatorSet(address _contractAddress);
+    event StakeContractSet(address _contractAddress);
+    event CoinsMinted(address indexed _stakeContract, address indexed _recipient, uint256 _mintAmount);
 
     modifier transfersNotFrozen() {
         require(!transfersFrozen);
@@ -48,7 +44,7 @@ contract RTCoin is Administration {
     }
 
     modifier onlyMinters() {
-        require(msg.sender == stakeContract || msg.sender == mergedMinerValidator);
+        require(msg.sender == stakeContractAddress || msg.sender == mergedMinerValidatorAddress);
         _;
     }
 
@@ -57,24 +53,99 @@ contract RTCoin is Administration {
         symbol = "RTC";
         decimals = 18;
         // 88il in wei
-        totalSupply = initialSupply;
+        totalSupply = INITIALSUPPLY;
         balances[msg.sender] = totalSupply;
         emit Transfer(address(0), msg.sender, totalSupply);
     }
 
-    function setMergedMinerValidator(address _mergedMinerValidator) external onlyAdmin returns (bool) {
-        mergedMinerValidator = _mergedMinerValidator;
+    function transfer(
+        address _recipient,
+        uint256 _amount
+    )
+        public
+        transfersNotFrozen
+        returns (bool transferred)
+    {
+        // check that the sender has a valid balance
+        require(balances[msg.sender] >= _amount);
+        balances[msg.sender] = balances[msg.sender].sub(_amount);
+        balances[_recipient] = balances[_recipient].add(_amount);
+        emit Transfer(msg.sender, _recipient, _amount);
         return true;
     }
 
+    function transferFrom(
+        address _owner,
+        address _recipient,
+        uint256 _amount
+    )
+        public
+        transfersNotFrozen
+        returns (bool transferredFrom)
+    {
+        // ensure owner has a valid balance
+        require(balances[_owner] >= _amount);
+        // ensure that the spender has a valid allowance
+        require(allowed[_owner][msg.sender] >= _amount);
+        require(allowed[_owner][msg.sender].sub(_amount) >= 0);
+        // reduce the allowance
+        allowed[_owner][msg.sender] = allowed[_owner][msg.sender].sub(_amount);
+        // reduce balance of owner
+        balances[_owner] = balances[_owner].sub(_amount);
+        // increase balance of recipient
+        balances[_recipient] = balances[_recipient].add(_amount);
+        emit Transfer(_owner, _recipient, _amount);
+        return true;
+    }
+
+    function approve(
+        address _spender,
+        uint256 _amount
+    )
+        public
+        returns (bool approved)
+    {
+        require(_spender != address(0x0));
+        require(_amount > 0);
+        require(allowed[msg.sender][_spender].add(_amount) > allowed[msg.sender][_spender]);
+        allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_amount);
+        emit Approval(msg.sender, _spender, _amount);
+        return true;
+    }
+
+    // NON STANDARD FUNCTIONS //
+
+    function setMergedMinerValidator(address _mergedMinerValidator) external onlyAdmin returns (bool) {
+        mergedMinerValidatorAddress = _mergedMinerValidator;
+        emit MergedMinerValidatorSet(_mergedMinerValidator);
+        return true;
+    }
+
+
     function setStakeContract(address _contractAddress) external onlyAdmin returns (bool) {
         // this prevents us from changing contracts while there are active stakes going on
-        if (stakeContract != address(0)) {
+        if (stakeContractAddress != address(0)) {
             require(stake.activeStakes() == 0);
         }
-        stakeContract = _contractAddress;
+        stakeContractAddress = _contractAddress;
         stake = StakeInterface(_contractAddress);
-        // event place holder
+        emit StakeContractSet(_contractAddress);
+        return true;
+    }
+
+
+    // This needs to be locked down so only the staking contract can invoke this function
+    function mint(
+        address _recipient,
+        uint256 _amount)
+        public
+        onlyMinters
+        returns (bool)
+    {
+        balances[_recipient] = balances[_recipient].add(_amount);
+        totalSupply = totalSupply.add(_amount);
+        emit Transfer(address(0), _recipient, _amount);
+        emit CoinsMinted(msg.sender, _recipient, _amount);
         return true;
     }
 
@@ -93,23 +164,7 @@ contract RTCoin is Administration {
         require(eI.transfer(_recipient, _amount));
         emit ForeignTokenTransfer(msg.sender, _recipient, _amount);
     }
-
-
-    // This needs to be locked down so only the staking contract can invoke this function
-    function mint(
-        address _recipient,
-        uint256 _amount)
-        public
-        onlyMinters
-        returns (bool)
-    {
-        balances[_recipient] = balances[_recipient].add(_amount);
-        totalSupply = totalSupply.add(_amount);
-        emit Transfer(address(0), _recipient, _amount);
-        // total supply increase place holder
-        return true;
-    }
-
+    
     //This will only ever have to be called in cases where a contract suicides and ether is forcably sent
     // or if someone sends ehter to the address the contract will reside at before it is deployed
     function transferOutEth()
@@ -142,72 +197,6 @@ contract RTCoin is Administration {
         return true;
     }
 
-    function transfer(
-        address _recipient,
-        uint256 _amount
-    )
-        public
-        transfersNotFrozen
-        returns (bool transferred)
-    {
-        require(transferCheck(msg.sender, _recipient, _amount));
-        balances[msg.sender] = balances[msg.sender].sub(_amount);
-        balances[_recipient] = balances[_recipient].add(_amount);
-        emit Transfer(msg.sender, _recipient, _amount);
-        return true;
-    }
-
-    function transferFrom(
-        address _owner,
-        address _recipient,
-        uint256 _amount
-    )
-        public
-        transfersNotFrozen
-        returns (bool transferredFrom)
-    {
-        require(transferCheck(_owner, _recipient, _amount));
-        require(allowed[_owner][msg.sender] >= _amount);
-        require(allowed[_owner][msg.sender].sub(_amount) >= 0);
-        allowed[_owner][msg.sender] = allowed[_owner][msg.sender].sub(_amount);
-        balances[_owner] = balances[_owner].sub(_amount);
-        balances[_recipient] = balances[_recipient].add(_amount);
-        emit Transfer(_owner, _recipient, _amount);
-        return true;
-    }
-
-    function approve(
-        address _spender,
-        uint256 _amount
-    )
-        public
-        returns (bool approved)
-    {
-        require(_spender != address(0x0));
-        require(_amount > 0);
-        require(allowed[msg.sender][_spender].add(_amount) > allowed[msg.sender][_spender]);
-        allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_amount);
-        emit Approval(msg.sender, _spender, _amount);
-        return true;
-    }
-
-    /**INTERNALS */
-
-    function transferCheck(
-        address _sender,
-        address _recipient,
-        uint256 _amount
-    )
-        internal
-        view
-        returns (bool valid)
-    {
-        require(_sender != address(0x0) && _recipient != address(0x0) && _amount > 0);
-        require(balances[_sender].sub(_amount) >= 0);
-        require(balances[_recipient].add(_amount) > 0);
-        require(balances[_recipient].add(_amount) > balances[_recipient]);
-        return true;
-    }
 
     /**GETTERS */
 
