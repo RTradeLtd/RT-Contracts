5ba222fe0b55a657e0eb85b777b83f6e2912b0e7
potsables <alextrottier2014@gmail.com>
2018-07-15 12:03:27 +0200

    working on bulk reward claim

--- RTCoin/MergedMiner.sol
+++ RTCoin/MergedMiner.sol
@@ -13,9 +13,6 @@ contract MinerValidator {
     address constant public TOKENADDRESS = 0xB8fe3B2C83014566733B766a27d94CB9AC167Dc6;
     RTCoinInterface constant public RTI = RTCoinInterface(TOKENADDRESS);
 
-
-    uint256[] public blockNumberArray;
-
     struct RewardStruct {
         uint256 totalRewards;
         uint256 balance;
@@ -74,7 +71,6 @@ contract MinerValidator {
         miners[block.coinbase].claims[block.number] = false;
         miners[block.coinbase].indexes[block.number] = miners[block.coinbase].blocks.length;
         blocks[block.number] = b;
-        blockNumberArray.push(block.number);
         require(RTI.transfer(msg.sender, SUBMISSIONREWARD), "failed to transfer reward to block submitter");
         return true;
     }
@@ -82,11 +78,47 @@ contract MinerValidator {
     function claimReward(uint256 _blockNumber) public isCoinbase(_blockNumber) unclaimed(_blockNumber) set(_blockNumber) returns (bool) {
         blocks[_blockNumber].claimed = true;
         miners[msg.sender].claims[block.number] = true;
-        uint256 index = miners[msg.sender].indexes[block.number];
+        uint256 index = miners[msg.sender].indexes[_blockNumber];
         // free up some space
         delete miners[msg.sender].blocks[index];
+        // set the claim status
+        miners[msg.sender].claims[_blockNumber] = true;
         require(RTI.transfer(msg.sender, BLOCKREWARD), "failed to transfer block reward");
         return true;
+    }
+
+    function bulkClaimReward(uint256[] _blocks) public returns (bool) {
+        uint256 cumulativeReward;
+        for (uint256 i = 0; i < _blocks.length; i++) {
+            uint256 num = _blocks[i];
+            // if this block has been claimed, skip processing
+            if (blocks[num].claimed) {
+                continue;
+            }
+            // if tx submitter isn't coinbase, skip processing
+            if (blocks[num].coinbase != msg.sender) {
+                continue;
+            }
+            // also check here that the block wasn't claimed
+            if (miners[msg.sender].claims[num]) {
+                continue;
+            }
+            // if the element is empty this has been claimed
+            if (miners[msg.sender].indexes[num] == 0) {
+                continue;
+            }
+            uint256 index = miners[msg.sender].indexes[num];
+            // free up some space
+            delete miners[msg.sender].blocks[index];
+            // set claimed to true
+            blocks[num].claimed = true;
+            // record the users claims
+            miners[msg.sender].claims[num] = true;
+            // update the cumulative reward
+            cumulativeReward = cumulativeReward.add(BLOCKREWARD);
+        }
+        require(cumulativeReward > 0, "no rewards recorded");
+        require(RTI.transfer(msg.sender, cumulativeReward), "failed to send tokens to claimer");
     }
 
     function getBlocksForMiner(address _miner) public view returns (uint256[]) {
