b5cefd943e6be47b3cdad8f0aab260e3fdf335ef
potsables <alextrottier2014@gmail.com>
2018-07-26 10:59:42 +0200

    payment channel smart contract work

--- TEMPORAL/Channels.sol
+++ TEMPORAL/Channels.sol
@@ -1,4 +1,5 @@
 pragma solidity 0.4.24;
+pragma experimental "v0.5.0";
 
 import "../Math/SafeMath.sol";
 import "../Modules/Administration.sol";
@@ -20,163 +21,89 @@ contract Channels is Administration {
     bytes constant private PREFIX = "\x19Ethereum Signed Message:\n32";
     address constant public RECIPIENT = address(0);
     address constant public TOKENADDRESS = address(0);
+    address constant public HOTWALLET = address(0);
+    uint256 constant private BLOCKACTIVITYLIMIT = 172800; // 172800 blocks
+    uint256 constant private TIMEACTIVITYLIMIT = 2592000; // 2592000 seconds
     RTCoinInterface constant public RTI = RTCoinInterface(TOKENADDRESS);
 
     // STATE CHANGEABLE
     address public admin;
 
     enum ChannelState { nil, opened, closed }
+    enum PaymentMethod { RTC, ETH }
 
     struct ChannelStruct {
-        address opener;
+        address owner;          // this is the owner/opener of the channel
         address recipient;      // this is the recipient of the channel, the person allowed to withdraw funds
-        uint256 openedAt;       // this is when the channel was opened
         uint256 balance;        // this is the current balance of the channel
-        uint256 lastWithdrawal; // this is the time at which a withdrawal was last made
         uint256 numberOfPayments; // this is usedto track the total number of payments made in this channel
+        uint256 lastBlockActivity; // this records the last block at which an action was performed on this channel
+        uint256 lastTimeActivity; // this records the last time at whichan action was performed
         ChannelState state;     // this is the current state of the channel
+        PaymentMethod method;   // this is the payment method
     }
 
     mapping (address => ChannelStruct) public channels;
 
-    event ChannelOpened(address _opener, address _recipient, uint256 _amount, uint256 _durationInSeconds);
+    event ChannelOpened(address _opener, address _recipient, uint256 _amount);
+
+    modifier inactiveChannel() {
+        require(checkForInactiveChannel(msg.sender));
+        _;
+    }
+
+    modifier validPaymentMethod(uint8 _method) {
+        require(PaymentMethod(_method) == PaymentMethod.RTC || PaymentMethod(_method) == PaymentMethod.ETH);
+        _;
+    }
+
+    // make sure the sender has neither an opened, nor closed channel
+    modifier noActiveChannel() {
+        require(channels[msg.sender].state == ChannelState.nil);
+        _;
+    }
 
     modifier onlyRecipient() {
         require(msg.sender == RECIPIENT);
         _;
     }
 
-    modifier noExistingChannel() {
-        require(channels[msg.sender].state == ChannelState.nil);
+    modifier onlyChannelOwner() {
+        require(msg.sender == channels[msg.sender].owner);
         _;
     }
 
-    modifier openedChannel(address _channelOpener) {
-        require(channels[_channelOpener].state == ChannelState.opened);
-        _;
-    }
-
-    modifier inactiveChannel() {
-        uint256 lastWithdrawal = channels[msg.sender].lastWithdrawal;
-        uint256 closureDate = lastWithdrawal.add(30 days);
-        require(now >= closureDate);
-        _;
-    }
-
-    modifier channelOpener() {
-        require(msg.sender == channels[msg.sender].opener);
-        _;
-    }
-
-    constructor() public {
-        // prevent deployments if the token address hasnt been setup
-        require(TOKENADDRESS != address(0), "token address not set");
-        // prevent deployments if recipient is not set
-        require(RECIPIENT != address(0), "recipient not set");
-        // prevent deployments if the admin is the recipient
-        require(msg.sender != RECIPIENT, "admin can't be the recipient");
-        admin = msg.sender;
+    function () payable external {
+        
     }
 
     function openChannel(
+        address _recipient,
         uint256 _channelBalance,
-        uint256 _durationInSeconds)
+        uint8   _paymentMethod)
         public
-        noExistingChannel
+        payable
+        noActiveChannel
+        validPaymentMethod(_paymentMethod)
         returns (bool)
     {
+        if (PaymentMethod(_paymentMethod) == PaymentMethod.ETH) {
+            require(msg.value == _channelBalance);
+        }
         ChannelStruct memory cs = ChannelStruct({
-            opener: msg.sender,
-            recipient: RECIPIENT,
-            openedAt: now,
+            owner: msg.sender,
+            recipient: _recipient,
             balance: _channelBalance,
-            lastWithdrawal: now,
             numberOfPayments: 0,
-            state: ChannelState.opened
+            lastBlockActivity: block.number,
+            lastTimeActivity: now,
+            state: ChannelState.opened,
+            method: PaymentMethod(_paymentMethod)
         });
         channels[msg.sender] = cs;
-        emit ChannelOpened(msg.sender, RECIPIENT, _channelBalance, _durationInSeconds);
-        require(RTI.transferFrom(msg.sender, address(this), _channelBalance), "transfer from failed, most likely needs approval");
-        return true;
-    }
-
-    // allows channel opener to close the chanel and withdraw their funds after the inactive period
-    function closeChannel() public channelOpener inactiveChannel returns (bool) {
-        uint256 balance = channels[msg.sender].balance;
-        channels[msg.sender].balance = 0;
-        channels[msg.sender].state = ChannelState.closed;
-        require(RTI.transfer(msg.sender, balance), "failed to close channel");
-        return true;
-    }
-
-    function withdrawFromChannel(
-        address _channelOpener,
-        bytes32 _h,
-        uint8   _v,
-        bytes32 _r,
-        bytes32 _s,
-        uint256 _amount,
-        uint256 _paymentNumber,
-        bool    _prefixed)
-        public
-        onlyRecipient
-        openedChannel(_channelOpener)
-        returns (bool)
-    {
-        require(_paymentNumber > channels[_channelOpener].numberOfPayments, "invalid payment number, less than what is in storage");
-        bytes32 image;
-        if (_prefixed) {
-            image = generatePreimage(_channelOpener, _amount, _paymentNumber);
-            image = generatePrefixedPreimage(image);
-        } else {
-            image = generatePreimage(_channelOpener, _amount, _paymentNumber);
+        if (PaymentMethod(_paymentMethod) == PaymentMethod.RTC) {
+            require(RTI.transferFrom(msg.sender, address(this), _channelBalance));
         }
-        require(image == _h, "constructed preimage does not match hash");
-        address signer = ecrecover(_h, _v, _r, _s);
-        require(signer == _channelOpener, "recovered signer is not channel opener");
-        require(channels[_channelOpener].balance >= _amount, "not enough channel balance");
-        channels[_channelOpener].balance = channels[_channelOpener].balance.sub(_amount);
-        channels[_channelOpener].numberOfPayments = _paymentNumber;
-        channels[_channelOpener].lastWithdrawal = now;
-        require(RTI.transfer(msg.sender, _amount), "failed to withdraw funds");
-        return true;
-    }
-
-    function verifyImageAndSigner(
-        address _channelOpener,
-        uint256 _amount,
-        uint256 _paymentNumber,
-        bool    _prefixed,
-        bytes32 _h,
-        uint8   _v,
-        bytes32 _r,
-        bytes32 _s)
-        public
-        view
-        returns (bool)
-    {
-        bytes32 image;
-        if (_prefixed) {
-            image = generatePreimage(_channelOpener, _amount, _paymentNumber);
-            image = generatePrefixedPreimage(image);
-        } else {
-            image = generatePreimage(_channelOpener, _amount, _paymentNumber);
-        }
-        require(image == _h, "reconstructed image does not match");
-        return _channelOpener == ecrecover(_h, _v, _r, _s);
-    }
-
-    // allows channel opener to replenish the channel balance
-    // do note that this will "reset" the last withdrawal date
-    function depositFundsIntoChannel(
-        uint256 _amount)
-        public
-        openedChannel(msg.sender)
-        returns (bool)
-    {
-        channels[msg.sender].balance = channels[msg.sender].balance.add(_amount);
-        channels[msg.sender].lastWithdrawal = now;
-        require(RTI.transferFrom(msg.sender, address(this), _amount), "transfer from failed, most likely needs approval");
         return true;
     }
 
@@ -193,5 +120,13 @@ contract Channels is Administration {
 
     function generatePrefixedPreimage(bytes32 _preimage) internal pure returns (bytes32)  {
         return keccak256(abi.encodePacked(PREFIX, _preimage));
+    }
+
+    function checkForInactiveChannel(address _channelOwner) public view returns (bool) {
+        uint256 inactiveBlockNumber = channels[_channelOwner].lastBlockActivity.add(BLOCKACTIVITYLIMIT);
+        require(block.number <= inactiveBlockNumber, "172800 blocks must've passed since last action");
+        uint256 inactivityDate = channels[_channelOwner].lastTimeActivity.add(TIMEACTIVITYLIMIT);
+        require(now <= inactivityDate, "2592000 seconds must've passed since last action");
+        return true;
     }
 }
